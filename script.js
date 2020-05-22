7f4667850e0eb69161ce657c45c18a81f58aa40b
Yulya Vorontsova <secondnumber01@gmail.com>
2020-05-22 17:00:14 +0300

    refactor(product): change task about ajax

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const list = document.querySelector('.result');
 const clickMeButton = document.querySelector('.click-me');
 const numberPage = document.querySelector('.number');
-clickMeButton.addEventListener('click', makeRequest);
+clickMeButton.addEventListener('click', () => {
+  getImages(numberPage.value, onDataReceived);
+});
 
-function makeRequest () {
-  $.ajax(`https://repetitora.net/api/JS/Images?page=${numberPage.value}&count=1`, {
-    success: function (data) {
-      data.forEach(element => {
-        const url = element.thumbnail;
-        const item = document.createElement('li');
-        const image = document.createElement('img');
-        image.src = url;
-        item.appendChild(image);
-        list.appendChild(item);
-      })
-    }
-  });
-};
+function onDataReceived (data) {
+  data.forEach(element => {
+    const url = element.thumbnail;
+    const item = document.createElement('li');
+    const image = document.createElement('img');
+    image.src = url;
+    item.appendChild(image);
+    list.appendChild(item);
+  })
+}
